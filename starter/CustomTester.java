f81b9adea8e66910c822e602f95a1dc94f1fdc5d
Jianwei Gong <Jinle.Gong2014@gmail.com>
2022-02-26 16:27:27 -0800

    ready

---
+++ starter/CustomTester.java
@@ -0,0 +1,7 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+// TODO Implement your custom test cases in this calss
+public class CustomTester {
+    
+}
---
+++ starter/MyCalendar.java
@@ -0,0 +1,28 @@
+/**
+ * TODO: Add your file header
+ * Name:
+ * ID:
+ * Email:
+ * Sources used: Put "None" if you did not have any external help
+ * Some example of sources used would be Tutors, Zybooks, and Lecture Slides
+ * 
+ * 2-4 sentence file description here
+ */
+
+ 
+public class MyCalendar {
+    public MyTreeMap<Integer, Integer> calender;
+    
+    public MyCalendar() {
+        // TODO
+    }
+    
+    public boolean book(int start, int end) {
+        // TODO 
+        return false;
+    }
+
+    public MyTreeMap getCalender(){
+        return this.calender;
+    }
+}
